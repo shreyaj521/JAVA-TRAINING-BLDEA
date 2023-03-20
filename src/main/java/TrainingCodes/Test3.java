8f6247cc86c17c90df4badc8c7e7235731d269b2
Dell <Dell@DESKTOP-TR50PVN>
2023-03-20 21:29:50 +0530

    third commit

---
+++ src/main/java/TrainingCodes/Test3.java
@@ -0,0 +1,21 @@
+package TrainingCodes;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count=1;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print(count+" ");
+				count++;
+			}
+			
+		System.out.println();
+		}	}
+
+		
+	}
+
+
